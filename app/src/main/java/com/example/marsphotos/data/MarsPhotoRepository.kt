cf6c63083048e1d14d01026c4f35190faf8163d0
Leonan Marques <leonan.marques886@gmail.com>
2024-02-11 12:25:26 -0300

    app container have been created

--- app/src/main/java/com/example/marsphotos/data/MarsPhotoRepository.kt
+++ app/src/main/java/com/example/marsphotos/data/MarsPhotoRepository.kt
@@ -1,14 +1,14 @@
 package com.example.marsphotos.data
 
-import com.example.marsphotos.network.MarsApi
+import com.example.marsphotos.network.MarsApiService
 import com.example.marsphotos.network.MarsPhoto
 
 interface MarsPhotoRepository {
   suspend fun getPhotos(): List<MarsPhoto>
 }
 
-class NetworkMarsPhotoRepository() : MarsPhotoRepository {
-  override suspend fun getPhotos(): List<MarsPhoto> {
-    return MarsApi.retrofitService.getPhotos()
-  }
+class NetworkMarsPhotoRepository(
+  private val marsApiService: MarsApiService
+) : MarsPhotoRepository {
+  override suspend fun getPhotos(): List<MarsPhoto> = marsApiService.getPhotos()
 }
